399d7c05187178163f6b4e47574405ffb1dec96a
gusmuaha <gusmuaha@student.gu.se>
2020-02-01 15:32:55 +0100

    Added work folder

---
+++ WP2/task2.c
@@ -0,0 +1,12 @@
+/* ====================================
+File name: task2.c (or cpp)
+Date: 2020-mm-dd
+Group nr xxx
+
+Members that contribute to the solutions
+Hassan Mualla
+Hannah Maltkvist
+
+Member not present at demonstration time:
+Demonstration code : [<Ass code 1-4> <abc>] Important, No code no exercise points!
+====================================== */
