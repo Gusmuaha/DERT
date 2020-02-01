de711fa2370cfff666a750cd22fd634e259b16a2
gusmuaha <gusmuaha@student.gu.se>
2020-02-01 17:23:27 +0100

    finished task 2

--- WP2/task2.c
+++ WP2/task2.c
@@ -10,3 +10,25 @@ Hannah Maltkvist
 Member not present at demonstration time:
 Demonstration code : [<Ass code 1-4> <abc>] Important, No code no exercise points!
 ====================================== */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+const int MAX = 10;
+int main() {
+  int arr[MAX];
+  for (int i = 0; i < MAX; i++) {
+    arr[i] = rand()%99;
+    printf("%d\n", arr[i]);
+  }
+  printf("The value of the label array(address) is: %pn \n", arr);
+  printf("First integer in the array is: %d \n", arr[0]);
+  printf("The size of an integer (number of bytes) is: %ld \n", sizeof(arr[0]));
+  printf("The size of the whole array is: %ld \n", sizeof(arr));
+  int *pointer;
+  for (int i = 0; i < (int)sizeof(arr)/4; i++){
+    pointer = &arr[i];
+    printf("%d \n", *pointer);
+  }
+  return 0;
+}
