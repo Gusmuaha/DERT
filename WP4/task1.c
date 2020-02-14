16b588668be91a6877ecd442b62cf165a6ac087a
Hannah Maltkvist <hannahmaltkvist@gmail.com>
2020-02-14 18:34:54 +0100

    WP4 folder

---
+++ WP4/task1.c
@@ -0,0 +1,16 @@
+/* ====================================
+File name: task1.c (or cpp)
+Date: 2020-mm-dd
+Group nr 17
+
+Members that contribute to the solutions
+Hassan Mualla
+Hannah Maltkvist
+Carl Nihlmar
+
+
+Member not present at demonstration time: 
+Demonstration code : []
+ Important, No code no exercise points!
+====================================== */
+
